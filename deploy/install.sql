91e04868106b366665fcbb1e17c286891a0362fa
JEE <etxegui@gmail.com>
2017-03-07 11:42:26 +0100

    bd vacia

--- deploy/install.sql
+++ deploy/install.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `tirada` (
   PRIMARY KEY (`id`),
   KEY `fk_tiradas_usuarios_idx` (`usuario_id`),
   CONSTRAINT `fk_tiradas_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=149 DEFAULT CHARSET=latin1;
 
 -- Volcando datos para la tabla dado_eq3.tirada: ~0 rows (aproximadamente)
 DELETE FROM `tirada`;
@@ -42,25 +42,11 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `fecha_modificacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `fecha_baja` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla dado_eq3.usuario: ~13 rows (aproximadamente)
+-- Volcando datos para la tabla dado_eq3.usuario: ~12 rows (aproximadamente)
 DELETE FROM `usuario`;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`id`, `nombre`, `fecha_alta`, `fecha_modificacion`, `fecha_baja`) VALUES
-	(9, 'Guillermo', '2017-03-03 10:51:29', '2017-03-06 13:18:48', NULL),
-	(10, 'Ivan', '2017-03-03 10:51:39', '2017-03-06 13:20:52', NULL),
-	(11, 'Josu', '2017-03-03 10:55:08', '2017-03-06 13:21:04', NULL),
-	(12, 'Ignacio', '2017-03-03 10:55:18', '2017-03-06 13:21:07', NULL),
-	(13, 'Aaron', '2017-03-03 11:55:32', '2017-03-06 13:21:10', NULL),
-	(16, 'Itsaso', '2017-03-03 12:51:43', '2017-03-06 13:21:14', NULL),
-	(17, 'Eneko', '2017-03-03 12:51:52', '2017-03-06 13:21:22', NULL),
-	(18, 'Gomez', '2017-03-03 12:52:01', '2017-03-06 13:21:26', NULL),
-	(19, 'Nagore', '2017-03-03 12:52:08', '2017-03-06 13:21:32', NULL),
-	(20, 'Garbiñe', '2017-03-03 12:52:15', '2017-03-06 13:21:35', NULL),
-	(21, 'Egoitz', '2017-03-03 12:52:25', '2017-03-06 13:21:38', NULL),
-	(22, 'Perez', '2017-03-03 12:52:33', '2017-03-06 13:21:41', NULL),
-	(25, 'Pavel', '2017-03-06 10:51:05', '2017-03-06 13:21:44', NULL);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
