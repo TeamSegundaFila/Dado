044aabc9b267a16b39d10927e690fe6a70b5e9a1
aloro <alozoyarodriguez@gmail.com>
2017-03-08 11:32:02 +0100

    Tests de pojos, cambio en pom para sonarqube, cambio en log4j para escribir log en fichero externo

---
+++ src/test/java/com/ipartek/formacion/domain/EstadisticaTest.java
@@ -0,0 +1,59 @@
+package com.ipartek.formacion.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EstadisticaTest {
+
+	Estadistica estadistica;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		try {
+			estadistica = new Estadistica();
+		} catch (Exception e) {
+			fail("No deberia haber fallado el SetUp de EstadisticaTest");
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		estadistica = null;
+	}
+
+	@Test
+	public void testConstructorVacioEstadistica() {
+		assertTrue(
+				"el nombre de estadistica generado por constructor vacío no coincide con el obtenido por Getter, pero deberia",
+				"".equals(estadistica.getNombre()));
+		assertEquals("el id generado por constructor vacío no coincide con el obtenido por Getter, pero deberia", -1,
+				estadistica.getContador());
+	}
+
+	@Test
+	public void testSettersEstadistica() {
+		estadistica.setNombre("nombreEstadisticaTest");
+		estadistica.setContador(0);
+		assertTrue("el nombre de estadistica generado por setter no coincide con el obtenido por Getter, pero deberia",
+				"nombreEstadisticaTest".equals(estadistica.getNombre()));
+		assertEquals(
+				"el contador de estadisticas guardado por Setter no coincide con el obtenido por Getter, pero deberia",
+				0, ((int) estadistica.getContador()));
+
+	}
+}
---
+++ src/test/java/com/ipartek/formacion/domain/LanzamientosTest.java
@@ -0,0 +1,68 @@
+package com.ipartek.formacion.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LanzamientosTest {
+
+	Lanzamientos lanzamientos;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		try {
+			lanzamientos = new Lanzamientos();
+		} catch (Exception e) {
+			fail("No deberia haber fallado el SetUp de LanzamientosTest");
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		lanzamientos = null;
+	}
+
+	@Test
+	public void testConstructorVacioLanzamientos() {
+
+		assertEquals("el id generado por constructor vacío no coincide con el obtenido por Getter, pero deberia", -1,
+				lanzamientos.getId());
+		assertTrue(
+				"el nombre de lanzamientos generado por constructor vacío no coincide con el obtenido por Getter, pero deberia",
+				"".equals(lanzamientos.getNombre()));
+		assertTrue(
+				"la fecha de lanzamientos generada por constructor vacío no coincide con el obtenido por Getter, pero deberia",
+				"".equals(lanzamientos.getFecha()));
+
+	}
+
+	@Test
+	public void testSettersEstadistica() {
+		lanzamientos.setId(0);
+		lanzamientos.setNombre("nombreLanzamientosTest");
+		lanzamientos.setFecha("fechaLanzamientosTest");
+
+		assertEquals("el id de Lanzamientos guardado por Setter no coincide con el obtenido por Getter, pero deberia",
+				0, ((int) lanzamientos.getId()));
+		assertTrue("el nombre de Lanzamientos generado por setter no coincide con el obtenido por Getter, pero deberia",
+				"nombreLanzamientosTest".equals(lanzamientos.getNombre()));
+		assertTrue("la fecha de Lanzamientos generada por setter no coincide con la obtenida por Getter, pero deberia",
+				"fechaLanzamientosTest".equals(lanzamientos.getFecha()));
+
+	}
+
+}
